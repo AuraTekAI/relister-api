9431a298c3acb64c46c26da764be832494c1c698
Basit Ali Sandhu <muhammadbasitali0@gmail.com>
2025-02-07 17:08:48 +0500

    Facebook import corrected

--- src/VehicleListing/tasks.py
+++ src/VehicleListing/tasks.py
@@ -2,7 +2,7 @@ from relister.celery import CustomExceptionHandler
 from celery import shared_task
 from VehicleListing.facebook_listing import create_marketplace_listing,login_to_facebook
 from VehicleListing.models import VehicleListing, FacebookListing
-from accounts.models import FacebookUserCredentials
+from .models import FacebookUserCredentials
 
 @shared_task(bind=True, base=CustomExceptionHandler,queue='scheduling_queue')
 def create_facebook_marketplace_listing_task(self):
